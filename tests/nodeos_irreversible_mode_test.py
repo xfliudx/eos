992fc1aa64a6ab0ba301cfdb58610a77b61aa174
Andrianto Lie <andrianto.lie@ii5.com>
2019-02-26 11:53:14 +0800

    Cleanup

--- tests/nodeos_irreversible_mode_test.py
+++ tests/nodeos_irreversible_mode_test.py
@@ -59,8 +59,11 @@ def getHeadBlockNumAndLib(node: Node):
    info = node.getInfo()
    headBlockNum = int(info["head_block_num"])
    lib = int(info["last_irreversible_block_num"])
-   return headBlockNum, lib
+   forkDbHeadBlockNum =  int(info["fork_db_head_block_num"])
+   return headBlockNum, lib, forkDbHeadBlockNum
 
+# List to contain the test result message
+testResultMsgs = []
 try:
    TestHelper.printSystemInfo("BEGIN")
    cluster.killall(allInstances=True)
@@ -87,7 +90,7 @@ try:
       clusterNode.kill(signal.SIGTERM)
    cluster.biosNode.kill(signal.SIGTERM)
 
-   def pauseBlockProduction():
+   def stopBlockProduction():
       if not producingNode.killed: producingNode.kill(signal.SIGTERM)
 
    def resumeBlockProduction():
@@ -96,14 +99,17 @@ try:
    # Wrapper function to execute test
    # This wrapper function will resurrect the node to be tested, and shut it down by the end of the test
    def executeTest(nodeIdOfNodeToTest, runTestScenario):
-      nodeToTest = cluster.getNode(nodeIdOfNodeToTest)
-      # Resurrect killed node to be tested
-      isRelaunchSuccess = nodeToTest.relaunch(nodeIdOfNodeToTest, "", timeout=relaunchTimeout)
-      assert(isRelaunchSuccess)
-      runTestScenario(nodeIdOfNodeToTest, nodeToTest)
-      # Kill node after use
-      if not nodeToTest.killed:
-        nodeToTest.kill(signal.SIGTERM)
+      try:
+         nodeToTest = cluster.getNode(nodeIdOfNodeToTest)
+         # Resurrect killed node to be tested
+         isRelaunchSuccess = nodeToTest.relaunch(nodeIdOfNodeToTest, "", timeout=relaunchTimeout)
+         assert isRelaunchSuccess, "Fail to relaunch before running test scenario"
+         runTestScenario(nodeIdOfNodeToTest, nodeToTest)
+         # Kill node after use
+         if not nodeToTest.killed: nodeToTest.kill(signal.SIGTERM)
+         testResultMsgs.append("!!!TEST CASE #{} IS SUCCESSFUL".format(nodeIdOfNodeToTest))
+      except Exception as e:
+         testResultMsgs.append("!!!BUG IS CONFIRMED ON TEST CASE #{} {}".format(nodeIdOfNodeToTest, e))
 
    # 1st test case: Replay in irreversible mode with reversible blocks
    # Bug: duplicate block added error
@@ -111,90 +117,66 @@ try:
       # Kill node and replay in irreversible mode
       nodeToTest.kill(signal.SIGTERM)
       isRelaunchSuccess = nodeToTest.relaunch(nodeIdOfNodeToTest, "--read-mode irreversible --replay", timeout=relaunchTimeout)
-
-      isStdErrContainMessage = False
-      if not isRelaunchSuccess:
-         isStdErrContainMessage = checkStdErr(nodeIdOfNodeToTest, "duplicate block added")
-      # TODO: Change this to proper assert
-      if not isRelaunchSuccess and isStdErrContainMessage:
-         Print("!!!BUG #1 CONFIRMED")
-      else:
-         Print("!!!BUG #1 IS NOT CONFIRMED")
+      assert isRelaunchSuccess, "Fail to relaunch"
 
    # 2nd test case: Replay in irreversible mode without reversible blocks
    # Bug: last_irreversible_block != the real lib (e.g. if lib is 1000, it replays up to 1000 saying head is 1000 and lib is 999)
    def replayInIrrModeWithoutRevBlocks(nodeIdOfNodeToTest, nodeToTest):
       # Track head block num and lib before shutdown
-      headBlockNumBeforeShutdown, libBeforeShutdown = getHeadBlockNumAndLib(nodeToTest)
+      headBlockNumBeforeShutdown, libBeforeShutdown, _ = getHeadBlockNumAndLib(nodeToTest)
       # Shut node, remove reversible blocks and relaunch
       nodeToTest.kill(signal.SIGTERM)
       removeReversibleBlocks(nodeIdOfNodeToTest)
       isRelaunchSuccess = nodeToTest.relaunch(nodeIdOfNodeToTest, "--replay --read-mode irreversible", timeout=relaunchTimeout)
-      assert(isRelaunchSuccess)
+      assert isRelaunchSuccess, "Fail to relaunch"
       # Check head block num and lib
-      headBlockNum, lib = getHeadBlockNumAndLib(nodeToTest)
+      headBlockNum, lib, _ = getHeadBlockNumAndLib(nodeToTest)
 
-      # TODO: Change this to proper assert
-      Print("headBlockNumBeforeShutdown {}, headBlockNum {}, lib {}, libBeforeShutdown {}".format(headBlockNumBeforeShutdown, headBlockNum, lib, libBeforeShutdown))
-      if not (headBlockNum == libBeforeShutdown and lib == libBeforeShutdown):
-         Print("!!!BUG #2 CONFIRMED")
-      else:
-         Print("!!!BUG #2 IS NOT CONFIRMED")
+      assert_msg = "headBlockNumBeforeShutdown {}, headBlockNum {}, lib {}, libBeforeShutdown {}".format(
+                   headBlockNumBeforeShutdown, headBlockNum, lib, libBeforeShutdown)
+      assert (headBlockNum == libBeforeShutdown and lib == libBeforeShutdown), assert_msg
 
    # 3rd test case: Switch mode speculative -> irreversible -> speculative without replay and production disabled
    def switchBackForthSpecAndIrrMode(nodeIdOfNodeToTest, nodeToTest):
-      # 3-1 Switch mode speculative -> irreversible -> speculative while production is disabled
+       # Track head block num and lib before shutdown
+      headBlockNumBeforeShutdown, libBeforeShutdown, forkDbBlockNumBeforeShutdown = getHeadBlockNumAndLib(nodeToTest)
       nodeToTest.kill(signal.SIGTERM)
       isRelaunchSuccess = nodeToTest.relaunch(nodeIdOfNodeToTest, "--read-mode irreversible", timeout=relaunchTimeout)
       assert(isRelaunchSuccess)
       nodeToTest.kill(signal.SIGTERM)
       isRelaunchSuccess = nodeToTest.relaunch(nodeIdOfNodeToTest, "", timeout=relaunchTimeout, addOrSwapFlags={"--read-mode": "speculative"})
       assert(isRelaunchSuccess)
-      Print("3RD TEST CASE SUCCESS")
+      # Check head block num and lib
+      headBlockNum, lib, forkDbBlockNum = getHeadBlockNumAndLib(nodeToTest)
+      Print("headBlockNumBeforeShutdown {}, headBlockNum {}, lib {}, libBeforeShutdown {}, forkDbBlockNum {}, forkDbBlockNumBeforeShutdown {}"
+         .format(headBlockNumBeforeShutdown, headBlockNum, lib, libBeforeShutdown, forkDbBlockNum, forkDbBlockNumBeforeShutdown))
+      assert_msg = "headBlockNumBeforeShutdown {}, headBlockNum {}, lib {}, libBeforeShutdown {}, forkDbBlockNum {}, forkDbBlockNumBeforeShutdown {}".format(
+                   headBlockNumBeforeShutdown, headBlockNum, lib, libBeforeShutdown, forkDbBlockNum, forkDbBlockNumBeforeShutdown)
+      assert (headBlockNum == libBeforeShutdown and lib == libBeforeShutdown and forkDbBlockNum == forkDbBlockNumBeforeShutdown), assert_msg
+
 
    # 4th test case: Switch mode speculative -> irreversible -> speculative without replay and production enabled
    def switchBackForthSpecAndIrrModeWithProdEnabled(nodeIdOfNodeToTest, nodeToTest):
       # Resume block production
       resumeBlockProduction()
-
       # Kill and relaunch in irreversible mode
       nodeToTest.kill(signal.SIGTERM)
       time.sleep(60) # Wait for some blocks to be produced and lib advance
       isRelaunchSuccess = nodeToTest.relaunch(nodeIdOfNodeToTest, "--read-mode irreversible", timeout=relaunchTimeout)
-
-      # Current code will throw a block_validate_exception "next block must be in the future"
-      isStdErrContainMessage = False
-      if not isRelaunchSuccess:
-         isStdErrContainMessage = checkStdErr(nodeIdOfNodeToTest, "next block must be in the future")
-      # TODO: Change this to proper assert
-      if not isRelaunchSuccess and isStdErrContainMessage:
-         Print("!!!BUG #4-1 CONFIRMED")
-      else:
-         Print("!!!BUG #4-1 IS NOT CONFIRMED")
-
-      # After it fails to relaunch, if we try to relaunch again, it will complain unlinkable block
-      isRelaunchSuccess = nodeToTest.relaunch(nodeIdOfNodeToTest, "--read-mode irreversible", timeout=relaunchTimeout)
-      isStdErrContainMessage = False
-      if not isRelaunchSuccess:
-         isStdErrContainMessage = checkStdErr(nodeIdOfNodeToTest, "unlinkable block")
-      # TODO: Change this to proper assert
-      if not isRelaunchSuccess and isStdErrContainMessage:
-         Print("!!!BUG #4-2 CONFIRMED")
-      else:
-         Print("!!!BUG #4-2 IS NOT CONFIRMED")
-
+      assert isRelaunchSuccess, "Fail to relaunch"
       # Stop block production
-      pauseBlockProduction()
+      stopBlockProduction()
 
+   # Start executing test cases here
    executeTest(1, replayInIrrModeWithRevBlocks)
    executeTest(2, replayInIrrModeWithoutRevBlocks)
    executeTest(3, switchBackForthSpecAndIrrMode)
    executeTest(4, switchBackForthSpecAndIrrModeWithProdEnabled)
 
-
 finally:
    TestHelper.shutdown(cluster, walletMgr)
    # walletMgr.killall(allInstances=True)
-   print("finally")
+   # Print test result
+   for msg in testResultMsgs: Print(msg)
 
 exit(0)
